15dea877cb937b6467a6c67695ce775fce15fe7d
Pranav Tondgaonkar <pranavrelds@gmail.com>
2023-07-01 04:22:23 +0530

    final commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+
+setup(name="Language_Identification_Using_CNN",
+    author_name="Pranav Tondgaonkar",
+    author_email="pranavrelds@gmail.com",
+    packages=find_packages(),
+    install_requires=[])
